0bffd9fb23acf1aa10a9d60576f8514b59839f17
Jeremy <jpierson@compunet.biz>
2019-01-14 20:15:24 -0700

    added indent

---
+++ indent-dedent.py
@@ -0,0 +1,22 @@
+
+#Pseudo-code example for indent
+
+
+awesome=True
+
+myvariable="someinterestingprogramming here"
+#perhaps you wrote this block of code and need to indent it to be under an if condition
+#you can select the entire block in IDLE and go to Format, Indent region to move it over
+
+if awesome=True:
+    myvariable="someinterestingprogramming here"
+    #perhaps you wrote this block of code and need to indent it to be under an if condition
+    #you can select the entire block in IDLE and go to Format, Indent region to move it over
+Else:
+    myvariable="muchlessinteresting"
+
+
+#Conversly, you can use Format, dedent to remove an indent that isn't needed any longer
+myvariable="someinterestingprogramming here"
+#perhaps you wrote this block of code and need to indent it to be under an if condition
+#you can select the entire block in IDLE and go to Format, Indent region to move it over
