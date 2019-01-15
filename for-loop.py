9eb6e0f8e170c73de13ad415008154d6ccd93876
Jeremy <jpierson@compunet.biz>
2019-01-14 20:04:47 -0700

    added for and if samples

---
+++ for-loop.py
@@ -0,0 +1,6 @@
+#sample for loop
+
+for i in range (1,5):
+    print("Hello "*i)
+
+print("Goodbye")
---
+++ if-else-boolean.py
@@ -0,0 +1,8 @@
+#sample for if else loop
+
+x=False
+
+if x == True:
+    print("It sure is true!")
+else:
+    print("It was a lie!")
